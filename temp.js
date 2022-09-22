9f13672d576041cab527d3d9daad01292499bf8e
huangzhihui <leononon412@gmail.com>
2022-09-22 23:22:09 +0800

    script demo

---
+++ temp.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         New script
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Zhihui
+// @match        *://*/*
+// @license      AGPL-3.0
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('hello world');
+    
+})();
